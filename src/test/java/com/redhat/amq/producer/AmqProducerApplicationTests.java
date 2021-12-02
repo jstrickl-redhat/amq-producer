5c983edff646a96026e25a5acc9d5cc178c3dd3c
Strickland, Joe <Stricj2@LabCorp.com>
2021-12-02 16:57:31 -0600

    init

---
+++ src/test/java/com/redhat/amq/producer/AmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.amq.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
